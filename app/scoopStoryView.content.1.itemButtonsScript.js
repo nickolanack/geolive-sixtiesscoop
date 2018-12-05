1736b3066e9d7758851448ac50be7e24dcb7b6ce
nickolanack <nickblackwell82@gmail.com>
2018-12-05 15:04:29 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/scoopStoryView.content.1.itemButtonsScript.js
@@ -0,0 +1,8 @@
+return [
+    new MockDataTypeItem({type:"story", id:-1}),
+    new MockDataTypeItem({type:"story", id:-1}),
+    new MockDataTypeItem({type:"story", id:-1}),
+    new MockDataTypeItem({type:"story", id:-1}),
+    new MockDataTypeItem({type:"story", id:-1}),
+    new MockDataTypeItem({type:"story", id:-1})
+    ];
