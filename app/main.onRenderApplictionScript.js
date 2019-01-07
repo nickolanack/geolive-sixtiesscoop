a6b330918266886588dd025433c0a0be259df4f1
nickolanack <nickblackwell82@gmail.com>
2019-01-07 19:28:30 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/main.onRenderApplictionScript.js
+++ app/main.onRenderApplictionScript.js
@@ -1,4 +1,6 @@
 GetWidget("mainStyle")->display($targetInstance);
+GetWidget("registerFormView")->display($targetInstance);
+GetWidget("loginFormView")->display($targetInstance);
 
 
 HtmlDocument()->META(HtmlDocument()->website(), 'base');
