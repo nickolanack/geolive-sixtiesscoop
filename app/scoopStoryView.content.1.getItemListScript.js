0bf6529bd0e9961f144b5ddbf7f11326c94b6c3c
nickolanack <nickblackwell82@gmail.com>
2019-01-08 06:47:18 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/scoopStoryView.content.1.getItemListScript.js
+++ app/scoopStoryView.content.1.getItemListScript.js
@@ -1,8 +1,1 @@
-return [
-    new StoryCard({id:-1, name:"Jon Brown"}),
-    new StoryCard({id:-1, name:"Vince Brown"}),
-    new StoryCard({id:-1, name:"Jon Brown"}),
-    new StoryCard({id:-1, name:"Jon Brown"}),
-    new StoryCard({id:-1, name:"Jon Brown"}),
-    new StoryCard({id:-1, name:"Jon Brown"})
-    ];
+ScoopStories.getCurrentCards(callback);
