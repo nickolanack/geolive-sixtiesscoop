d970f236c7bbe9f60b8ced32b8f313180b432c83
nickolanack <nickblackwell82@gmail.com>
2019-01-25 21:20:49 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/main.onRenderApplictionScript.js
+++ app/main.onRenderApplictionScript.js
@@ -5,7 +5,14 @@ GetWidget("profileFormView")->display($targetInstance);
 //GetWidget("addCardView")->display($targetInstance);
 //GetWidget("createStoryForm")->display($targetInstance);
 
+IncludeJSBlock('
 
+
+TemplateModule.SetTemplate(\'form\',\'<div><div data-template="title" class="template-title"></div><div data-template="content" class="template-content"></div><div data-template="footer" class="template-footer"></div></div>\');
+TemplateModule.SetTemplate(\'default\',\'<div data-template="content" class="template-content"></div>\');
+
+
+');
 
 IncludeJSBlock('
 
