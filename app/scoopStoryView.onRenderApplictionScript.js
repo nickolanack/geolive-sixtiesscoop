4d460a5c5fea40f6625b0a426dc4ef7daace64ba
nickolanack <nickblackwell82@gmail.com>
2019-01-26 16:32:31 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/scoopStoryView.onRenderApplictionScript.js
+++ app/scoopStoryView.onRenderApplictionScript.js
@@ -1,2 +1,4 @@
 GetWidget("addCardView")->display($targetInstance);
 GetWidget("createStoryForm")->display($targetInstance);
+GetWidget("createBirthStoryForm")->display($targetInstance);
+GetWidget("createRepatrationStoryForm")->display($targetInstance);
