0de0e178073b27f6cf3cf2a645fef2fc0beafd75
nickolanack <nickblackwell82@gmail.com>
2019-01-26 22:19:18 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/inline-birth-form.onRenderApplictionScript.js
@@ -0,0 +1,13 @@
+$apikey=GetPlugin('Maps')->getParameter('googleMapsServerApiKey', '');
+        if(!empty($apikey)){
+            $apikey='&key='.$apikey;
+        }
+        
+        $version=GetPlugin('Maps')->getParameter('googleMapsVersion', '');
+        if(!empty($version)){
+            $version='&v='.$version;
+        }
+
+        IncludeExternalJS(
+            '//maps.google.com/maps/api/js?libraries=places'.$apikey.$version
+        );
