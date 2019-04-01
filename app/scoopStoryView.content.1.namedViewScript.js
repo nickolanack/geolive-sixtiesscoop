9b74fcfd1c587e5e9e4e0a9370d2dff6f1b92ed3
nickolanack <nickblackwell82@gmail.com>
2019-04-01 02:11:34 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/scoopStoryView.content.1.namedViewScript.js
+++ app/scoopStoryView.content.1.namedViewScript.js
@@ -1,4 +1,10 @@
 if(item instanceof AddCard){
     return "addCardView";
 }
+if(item instanceof ProfileSummaryCard){
+    return "profileSummaryCardView";
+}
+if(item instanceof SortCard){
+    return "sortCardView";
+}
 return namedView
