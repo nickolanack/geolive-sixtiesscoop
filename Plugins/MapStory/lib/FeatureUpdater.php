904e9f29e1f486e4094ad40f0896f44a6ad2995c
Nicholas  Blackwell <nblackwe@Nicholass-Mac-mini.local>
2019-04-23 19:25:42 -0700

    refactor method

--- Plugins/MapStory/lib/FeatureUpdater.php
+++ Plugins/MapStory/lib/FeatureUpdater.php
@@ -5,14 +5,23 @@ namespace MapStory;
 
 class FeatureUpdater{
 
+	protected $feature=null;
+
 	protected function getPlugin(){
 		return GetPlugin('MapStory');
+	}
+
+	protected function getFeature($json){
+		if(is_null($this->feature)){
+			$this->feature=$this->getPlugin()->getUsersStoryMarker($json->id);
+		}
+		return $this->feature;
 	}
 
 	public function fromObject($json){
 
 
-		$feature=$this->getPlugin()->getUsersStoryMarker($json->id);
+		$feature=$this->getFeature($json);
 
 		if($feature->getId()<=0&&empty($json->location)){
 			throw new \Exception('Requires location for new items: '.$feature->getId());
@@ -34,6 +43,42 @@ class FeatureUpdater{
 		$defaultIcon=UrlFrom(GetWidget('siteConfig')->getParameter('storyIcon')[0] . '?thumb=>48>48');
 		$feature->setIcon($defaultIcon);
 
+		$this->setIcon($json);
+
+
+		(new \spatial\FeatureLoader())->save($feature);
+
+
+		$this->setAttributes($json);
+		
+
+		return $feature;
+
+	}
+
+	protected function setAttributes($json){
+
+		$feature=$this->getFeature($json);
+
+		GetPlugin('Attributes');
+		if (key_exists('attributes', $json)) {
+			foreach ($json->attributes as $table => $fields) {
+				(new \attributes\Record($table))->setValues($feature->getId(), "MapStory.card", $fields);
+			}
+		}
+
+		(new \attributes\Record($table))->setValues($feature->getId(), "MapStory.card", array(
+			"locationName"=>$json->address
+		));
+	}
+
+	protected function setIcon($json){
+
+		$feature=$this->getFeature($json);
+
+		$defaultIcon=UrlFrom(GetWidget('siteConfig')->getParameter('storyIcon')[0] . '?thumb=>48>48');
+		$feature->setIcon($defaultIcon);
+
 		if (key_exists('attributes', $json)){
 			if(key_exists('storyAttributes', $json->attributes)){
 				$attrs=$json->attributes->storyAttributes;
@@ -49,22 +94,6 @@ class FeatureUpdater{
 			}
 		}
 
-
-		(new \spatial\FeatureLoader())->save($feature);
-
-
-		GetPlugin('Attributes');
-		if (key_exists('attributes', $json)) {
-			foreach ($json->attributes as $table => $fields) {
-				(new \attributes\Record($table))->setValues($feature->getId(), "MapStory.card", $fields);
-			}
-		}
-
-		(new \attributes\Record($table))->setValues($feature->getId(), "MapStory.card", array(
-			"locationName"=>$json->address
-		));
-
-		return $feature;
 
 	}
 
