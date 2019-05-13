b76f321527a9e730a3ae765de39b991b634759fb
nickolanack <nickblackwell82@gmail.com>
2019-05-13 18:22:42 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/Adoption Visualization.customScript.js
+++ app/Adoption Visualization.customScript.js
@@ -1,5 +1,11 @@
-map.resetView();
-map.getLayerManager.getLayers().forEach(function(layer) {
-    layer.hide();
-});
+tile.addEvent('click',function(){
+    
+    
+    map.resetView();
+    map.getLayerManager().getLayers().forEach(function(layer) {
+        layer.hide();
+    });
+    
+})
+
 
