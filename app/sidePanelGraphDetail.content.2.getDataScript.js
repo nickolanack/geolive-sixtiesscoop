403dd0a54c3d670d9e01a5295c50838d4b0bf203
nickolanack <nickblackwell82@gmail.com>
2019-05-29 20:02:39 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/sidePanelGraphDetail.content.2.getDataScript.js
+++ app/sidePanelGraphDetail.content.2.getDataScript.js
@@ -1,8 +1,8 @@
-callback({
+callback(
     
         
-            result:[300, 350, 300, 0, 0, 0]
+           {"result": [{"value": [{"product.name": "apps", "result": 53}, {"product.name": "books", "result": 47}, {"product.name": "games", "result": 24}, {"product.name": "sounds", "result": 76}], "timeframe": {"start": "2019-03-20T00:00:00.000Z", "end": "2019-03-21T00:00:00.000Z"}}, {"value": [{"product.name": "apps", "result": 32}, {"product.name": "books", "result": 24}, {"product.name": "games", "result": 56}, {"product.name": "sounds", "result": 32}], "timeframe": {"start": "2019-03-21T00:00:00.000Z", "end": "2019-03-22T00:00:00.000Z"}}, {"value": [{"product.name": "apps", "result": 27}, {"product.name": "books", "result": 32}, {"product.name": "games", "result": 18}, {"product.name": "sounds", "result": 33}], "timeframe": {"start": "2019-03-22T00:00:00.000Z", "end": "2019-03-23T00:00:00.000Z"}}, {"value": [{"product.name": "apps", "result": 68}, {"product.name": "books", "result": 56}, {"product.name": "games", "result": 65}, {"product.name": "sounds", "result": 59}], "timeframe": {"start": "2019-03-23T00:00:00.000Z", "end": "2019-03-24T00:00:00.000Z"}}, {"value": [{"product.name": "apps", "result": 38}, {"product.name": "books", "result": 48}, {"product.name": "games", "result": 50}, {"product.name": "sounds", "result": 26}], "timeframe": {"start": "2019-03-24T00:00:00.000Z", "end": "2019-03-25T00:00:00.000Z"}}, {"value": [{"product.name": "apps", "result": 34}, {"product.name": "books", "result": 15}, {"product.name": "games", "result": 18}, {"product.name": "sounds", "result": 14}], "timeframe": {"start": "2019-03-25T00:00:00.000Z", "end": "2019-03-26T00:00:00.000Z"}}]}
           
         
-    }
+    
 );
