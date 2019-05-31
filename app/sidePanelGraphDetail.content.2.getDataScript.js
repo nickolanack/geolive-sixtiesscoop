95b1c1571c75ffa85461d05d9fc20a251d535dec
nickolanack <nickblackwell82@gmail.com>
2019-05-31 15:24:17 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/sidePanelGraphDetail.content.2.getDataScript.js
+++ app/sidePanelGraphDetail.content.2.getDataScript.js
@@ -5,8 +5,8 @@ setTimeout(function(){
                ([60, 61, 62, 63, 64, 65, 66]).map(function(yr){
                   
                   return  {"value": 
-                      item.getProviceCodeItems().map(function(item){
-                          return {"province.name": item.getTitle(), "result": Math.floor(Math.random() * Math.floor(10))};
+                      item.getProviceCodeItems().slice(0,4).map(function(item){
+                          return {"product.name": item.getTitle(), "result": Math.floor(Math.random() * Math.floor(10))};
                       }), 
                "timeframe": {"start": (1900+yr)+"-01-01T00:00:00.000Z", "end": (1900+yr+1)+"-01-01T00:00:00.000Z"}};
                    
