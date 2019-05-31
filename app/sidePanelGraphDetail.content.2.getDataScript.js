f2487a3d2146db64c6ea9d5ce816399cd0c7796f
nickolanack <nickblackwell82@gmail.com>
2019-05-31 15:27:03 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/sidePanelGraphDetail.content.2.getDataScript.js
+++ app/sidePanelGraphDetail.content.2.getDataScript.js
@@ -14,6 +14,116 @@ setTimeout(function(){
                
                ]}
     
-callback(data);
+callback({
+	"result": [{
+		"value": [{
+			"product.name": "BC",
+			"result": 53
+		}, {
+			"product.name": "AB",
+			"result": 47
+		}, {
+			"product.name": "SK",
+			"result": 24
+		}, {
+			"product.name": "MB",
+			"result": 76
+		}],
+		"timeframe": {
+			"start": "2019-03-20T00:00:00.000Z",
+			"end": "2019-03-21T00:00:00.000Z"
+		}
+	}, {
+		"value": [{
+			"product.name": "BC",
+			"result": 32
+		}, {
+			"product.name": "AB",
+			"result": 24
+		}, {
+			"product.name": "SK",
+			"result": 56
+		}, {
+			"product.name": "MB",
+			"result": 32
+		}],
+		"timeframe": {
+			"start": "2019-03-21T00:00:00.000Z",
+			"end": "2019-03-22T00:00:00.000Z"
+		}
+	}, {
+		"value": [{
+			"product.name": "BC",
+			"result": 27
+		}, {
+			"product.name": "AB",
+			"result": 32
+		}, {
+			"product.name": "SK",
+			"result": 18
+		}, {
+			"product.name": "MB",
+			"result": 33
+		}],
+		"timeframe": {
+			"start": "2019-03-22T00:00:00.000Z",
+			"end": "2019-03-23T00:00:00.000Z"
+		}
+	}, {
+		"value": [{
+			"product.name": "BC",
+			"result": 68
+		}, {
+			"product.name": "AB",
+			"result": 56
+		}, {
+			"product.name": "SK",
+			"result": 65
+		}, {
+			"product.name": "MB",
+			"result": 59
+		}],
+		"timeframe": {
+			"start": "2019-03-23T00:00:00.000Z",
+			"end": "2019-03-24T00:00:00.000Z"
+		}
+	}, {
+		"value": [{
+			"product.name": "BC",
+			"result": 38
+		}, {
+			"product.name": "AB",
+			"result": 48
+		}, {
+			"product.name": "SK",
+			"result": 50
+		}, {
+			"product.name": "MB",
+			"result": 26
+		}],
+		"timeframe": {
+			"start": "2019-03-24T00:00:00.000Z",
+			"end": "2019-03-25T00:00:00.000Z"
+		}
+	}, {
+		"value": [{
+			"product.name": "BC",
+			"result": 34
+		}, {
+			"product.name": "AB",
+			"result": 15
+		}, {
+			"product.name": "SK",
+			"result": 18
+		}, {
+			"product.name": "MB",
+			"result": 14
+		}],
+		"timeframe": {
+			"start": "2019-03-25T00:00:00.000Z",
+			"end": "2019-03-26T00:00:00.000Z"
+		}
+	}]
+});
 }, 100);
 
