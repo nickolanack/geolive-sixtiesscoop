749abf62870329f394b617c0c8e845ab2122c368
nickolanack <nickblackwell82@gmail.com>
2019-05-31 15:05:38 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/sidePanelGraphDetail.content.2.getDataScript.js
+++ app/sidePanelGraphDetail.content.2.getDataScript.js
@@ -2,7 +2,19 @@ setTimeout(function(){
 callback(
     
         
-           {"result": [{"value": [{"product.name": "apps", "result": 53}, {"product.name": "books", "result": 47}, {"product.name": "games", "result": 24}, {"product.name": "sounds", "result": 76}], "timeframe": {"start": "2019-03-20T00:00:00.000Z", "end": "2019-03-21T00:00:00.000Z"}}, {"value": [{"product.name": "apps", "result": 32}, {"product.name": "books", "result": 24}, {"product.name": "games", "result": 56}, {"product.name": "sounds", "result": 32}], "timeframe": {"start": "2019-03-21T00:00:00.000Z", "end": "2019-03-22T00:00:00.000Z"}}, {"value": [{"product.name": "apps", "result": 27}, {"product.name": "books", "result": 32}, {"product.name": "games", "result": 18}, {"product.name": "sounds", "result": 33}], "timeframe": {"start": "2019-03-22T00:00:00.000Z", "end": "2019-03-23T00:00:00.000Z"}}, {"value": [{"product.name": "apps", "result": 68}, {"product.name": "books", "result": 56}, {"product.name": "games", "result": 65}, {"product.name": "sounds", "result": 59}], "timeframe": {"start": "2019-03-23T00:00:00.000Z", "end": "2019-03-24T00:00:00.000Z"}}, {"value": [{"product.name": "apps", "result": 38}, {"product.name": "books", "result": 48}, {"product.name": "games", "result": 50}, {"product.name": "sounds", "result": 26}], "timeframe": {"start": "2019-03-24T00:00:00.000Z", "end": "2019-03-25T00:00:00.000Z"}}, {"value": [{"product.name": "apps", "result": 34}, {"product.name": "books", "result": 15}, {"product.name": "games", "result": 18}, {"product.name": "sounds", "result": 14}], "timeframe": {"start": "2019-03-25T00:00:00.000Z", "end": "2019-03-26T00:00:00.000Z"}}]}
+          return  {"result": [
+               
+               ([60, 61, 62, 63, 64, 65, 66]).map(function(yr){
+                  
+                  return  {"value": 
+                      item.getProvinceCodes().map(function(code){
+                          return {"province": code, "result": Math.floor(Math.random() * Math.floor(50))};
+                      }), 
+               "timeframe": {"start": "+(1900+yr)+"-01-01T00:00:00.000Z", "end": "+(1900+yr+1)+"-01-01T00:00:00.000Z"}};
+                   
+               })
+               
+               ]}
           
         
     
