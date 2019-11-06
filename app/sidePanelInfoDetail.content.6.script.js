8f185ee1c9b6613fe4a3961efe05d881c7db5f85
nickolanack <nickblackwell82@gmail.com>
2019-11-06 19:47:22 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/sidePanelInfoDetail.content.6.script.js
+++ app/sidePanelInfoDetail.content.6.script.js
@@ -1,6 +1,43 @@
 
 if(item.canEdit&&item.canEdit()){
-var btn=new Element('button', {
+    
+    
+  var deleteBtn=new Element('button', {
+                    html: "edit",
+                    "class": "inline-btn delete"
+                    events: {
+                        click: function(e) {
+                            (new UIModalDialog(application, {
+                                name: "Confirmation",
+                                description: "Are you sure you want to delete this card"
+                            }, {
+                                "formName": "dialogForm",
+                                "formOptions": {
+                                    "template": "form",
+                                    "className": "confirm-view"
+                                }
+                            })).show(function(answer) {
+
+
+                                if (answer == true) {
+
+                                    (new AjaxControlQuery(CoreAjaxUrlRoot, "delete_story_item", {
+                                        'plugin': 'MapStory',
+                                        'id': item.getId(),
+                                        'type': item.getType()
+                                    })).addEvent("success", function(resp) {
+                                        ScoopStories.clearCards()
+                                        ScoopStories.redraw();
+                                    }).execute();
+
+                                }
+
+                            });
+                        }
+                    }
+                })
+    
+var editBtn=new Element('button', {
                 html: "edit",
                 "class": "inline-btn edit"
             })
@@ -9,6 +46,12 @@ new UIModalFormButton(btn, ScoopStories.getMap(), item, {
                 formName: item.getFormView()
 
             });
-            return btn
+            
+            var span=new Element('span');
+            span.appendChild(deleteBtn);
+            span.appendChild(editBtn);
+            
+            
+            return span
 }
 return null;
