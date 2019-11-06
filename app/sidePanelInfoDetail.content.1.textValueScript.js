24cae8858b756717621a21b389cc701253f7c049
nickolanack <nickblackwell82@gmail.com>
2019-11-06 19:37:51 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/sidePanelInfoDetail.content.1.textValueScript.js
+++ app/sidePanelInfoDetail.content.1.textValueScript.js
@@ -1,1 +1,6 @@
-callback((item.isBirthStory&&item.isBirthStory())?'':item.getDescription());
+
+if(item instanceof StoryCard){
+    return "<h2>"+item.getTypeOfCard()+" Story</h2>";
+        
+}
+return "<h2>"+item.getTypeOfCard()+"</h2>";
