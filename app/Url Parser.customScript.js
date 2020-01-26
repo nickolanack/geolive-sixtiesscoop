668216110b74a109a2c58b39d2c661965801db4f
nickolanack <nickblackwell82@gmail.com>
2020-01-26 22:12:11 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/Url Parser.customScript.js
@@ -0,0 +1,16 @@
+if(window.location.href.indexOf('/tutorial')>0){
+    
+    application.getNamedObject('tutorial', function(tut){
+        tut.start();
+    })
+    
+    return;
+}
+
+
+if(window.location.href.indexOf('/register')>0){
+    
+    
+    
+    return;
+}
