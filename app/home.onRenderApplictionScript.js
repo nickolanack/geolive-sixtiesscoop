3f389d2788317e38c5713f0d7b49b9f133a21943
nickolanack <nickblackwell82@gmail.com>
2022-09-29 17:13:06 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/home.onRenderApplictionScript.js
+++ app/home.onRenderApplictionScript.js
@@ -1,2 +1,5 @@
-TemplateModule.SetTemplate(\'default\',\'<div data-template="content" class="template-content"></div>\');
+IncludeJSBlock('
+    TemplateModule.SetTemplate(\'default\',\'<div data-template="content" class="template-content"></div>\');
+');
+
 GetWidget("homeStyle")->display($targetInstance);
