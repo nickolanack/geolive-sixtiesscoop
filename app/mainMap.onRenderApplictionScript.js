fa13dacb8049d51ef253b994bb154874f724d21d
nickolanack <nickblackwell82@gmail.com>
2022-09-29 21:12:47 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/mainMap.onRenderApplictionScript.js
@@ -0,0 +1,5 @@
+IncludeJSBlock('
+    TemplateModule.SetTemplate(\'default\',\'<div data-template="content" class="template-content"></div>\');
+');
+
+GetWidget("homeStyle")->display($targetInstance);
