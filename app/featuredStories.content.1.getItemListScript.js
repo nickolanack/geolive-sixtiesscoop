7335d21afe6e5a6a34b63e5100a9e059a1d988b0
nickolanack <nickblackwell82@gmail.com>
2022-09-29 18:56:58 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/featuredStories.content.1.getItemListScript.js
+++ app/featuredStories.content.1.getItemListScript.js
@@ -1,1 +1,5 @@
-callback([AppClient, AppClient, AppClient]);
+callback([
+    new MockDataTypeItem({name:"##Example 1"}), 
+    new MockDataTypeItem({name:"##Example 2"}), 
+    new MockDataTypeItem({name:"##Example 2"})
+]);
