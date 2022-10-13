234733a93dc90f59e8e78a941297744ba2049f8e
nickolanack <nickblackwell82@gmail.com>
2022-10-13 23:19:13 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/home.onRenderApplictionScript.js
+++ app/home.onRenderApplictionScript.js
@@ -1,4 +1,5 @@
 IncludeJSBlock('
+    TemplateModule.SetTemplate(\'form\',\'<div><div data-template="title" class="template-title"></div><div data-template="content" class="template-content"></div><div data-template="footer" class="template-footer"></div></div>\');
     TemplateModule.SetTemplate(\'default\',\'<div data-template="content" class="template-content"></div>\');
 ');
 
