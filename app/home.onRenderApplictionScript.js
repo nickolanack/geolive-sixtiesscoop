816450dd004a797fec1f6d9d4a329c9f700b31bf
nickolanack <nickblackwell82@gmail.com>
2022-10-13 22:43:36 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/home.onRenderApplictionScript.js
+++ app/home.onRenderApplictionScript.js
@@ -2,5 +2,7 @@ IncludeJSBlock('
     TemplateModule.SetTemplate(\'default\',\'<div data-template="content" class="template-content"></div>\');
 ');
 
+IncludeJS('{widgets}/WizardBuilder/js/FormBuilder.js');
+
 GetWidget("homeStyle")->display($targetInstance);
 GetWidget("genericParametersForm")->display($targetInstance);
