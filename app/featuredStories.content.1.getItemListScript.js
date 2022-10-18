df1f524d501e14b85def4b6cf410640030d7ffc4
nickolanack <nickblackwell82@gmail.com>
2022-10-18 20:04:34 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/featuredStories.content.1.getItemListScript.js
+++ app/featuredStories.content.1.getItemListScript.js
@@ -1,5 +1,20 @@
-callback([
-    new MockDataTypeItem({name:"##Example 1"}), 
-    new MockDataTypeItem({name:"##Example 2"}), 
-    new MockDataTypeItem({name:"##Example 2"})
-]);
+
+
+    var defaultList=[
+        new MockDataTypeItem({name:"##Example 1"}), 
+        new MockDataTypeItem({name:"##Example 2"}), 
+        new MockDataTypeItem({name:"##Example 2"})
+    ];
+
+
+
+	(new AjaxControlQuery(CoreAjaxUrlRoot, "get_configuration_field", {
+		'widget': "featuredStoriesItems",
+		'field': "featured"
+	})).addEvent('success',function(response){
+        callback(defaultList);
+	}).execute();
+
+
+
+
