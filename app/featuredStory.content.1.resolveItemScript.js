74e53aa85aec596d7ee94d85ed9004f2d029b684
nickolanack <nickblackwell82@gmail.com>
2022-10-19 05:14:09 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/featuredStory.content.1.resolveItemScript.js
+++ app/featuredStory.content.1.resolveItemScript.js
@@ -1,1 +1,19 @@
-return item.getCards()[item.getCard()];
+
+
+ (new AjaxControlQuery(CoreAjaxUrlRoot, "get_story_with_item", {
+                    "plugin": "MapStory",
+                    "item": item.getId()
+                })).addEvent("success", function(resp) {
+                  
+                    var story=new StoryUser(resp);
+                
+                    
+                    story.getCards(function(cards){
+                        callback(cards[item.getCard()]);
+                    });
+                    
+                 
+                    
+                }).execute();
+
+
