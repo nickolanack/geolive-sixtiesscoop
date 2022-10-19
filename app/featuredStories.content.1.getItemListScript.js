67b8e80ccf084e1e05584f890da1e9c82d92d333
nickolanack <nickblackwell82@gmail.com>
2022-10-19 02:14:49 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/featuredStories.content.1.getItemListScript.js
+++ app/featuredStories.content.1.getItemListScript.js
@@ -13,14 +13,35 @@
 		'field': "featured"
 	})).addEvent('success',function(response){
 	    
-	       var list=response.value.map(function(data, i){
+	       var list;
+	       var check=function(){
+	           
+	       };
+	    
+	       list=response.value.map(function(data, i){
 	           
 	           if(data.name){
 	               data.name='##'+data.name;
 	           }
 	           
-	           return new MockDataTypeItem(ObjectAppend_({name:"##Item "+i}, data));
+	           var item = new MockDataTypeItem(ObjectAppend_({mutable:true, name:"##Item "+i, story:null}, data));
+	           
+	           (new AjaxControlQuery(CoreAjaxUrlRoot, "get_story_with_item", {
+                    "plugin": "MapStory",
+                    "item": 335
+                })).addEvent("success", function(resp) {
+                  
+                    item.setStory(new StoryUser(resp));
+                    check();
+                    
+                }).execute();
+                
+	           
+	           return item;
 	       });
+	       
+           
+	       
 	    
         callback(list.concat(defaultList).slice(0,3));
         
