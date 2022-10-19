397457a0b677fb4d0b12d0f799a7b8e46568cdb2
nickolanack <nickblackwell82@gmail.com>
2022-10-19 04:10:13 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/featuredStories.content.1.getItemListScript.js
+++ app/featuredStories.content.1.getItemListScript.js
@@ -1,10 +1,10 @@
 
 
-    var defaultList=[
-        new MockDataTypeItem({name:"##Example 1"}), 
-        new MockDataTypeItem({name:"##Example 2"}), 
-        new MockDataTypeItem({name:"##Example 2"})
-    ];
+    // var defaultList=[
+    //     new MockDataTypeItem({name:"##Example 1"}), 
+    //     new MockDataTypeItem({name:"##Example 2"}), 
+    //     new MockDataTypeItem({name:"##Example 2"})
+    // ];
 
 
 
@@ -13,25 +13,32 @@
 		'field': "featured"
 	})).addEvent('success',function(response){
 	    
-	       var list;
+	       var list=[];
 	       var check=function(){
-	           
-	       };
-	    
-	       list=response.value.map(function(data, i){
-	           
-	           if(data.name){
-	               data.name='##'+data.name;
+	           if(list.length==3){
+	               callback(list);
 	           }
+	       };
+	       
+	    
+	       list=[335, 312, 307].value.map(function(id, i){
+	           
+	           //if(data.name){
+	           //    data.name='##'+data.name;
+	           //}
+	           
+	           var data = {};
+	
 	           
 	           var item = new MockDataTypeItem(ObjectAppend_({mutable:true, name:"##Item "+i, story:null}, data));
 	           
-	           (new AjaxControlQuery(CoreAjaxUrlRoot, "get_story_with_item", {
+	            (new AjaxControlQuery(CoreAjaxUrlRoot, "get_story_with_item", {
                     "plugin": "MapStory",
-                    "item": 335
+                    "item": id
                 })).addEvent("success", function(resp) {
                   
                     item.setStory(new StoryUser(resp));
+                    list.push(item);
                     check();
                     
                 }).execute();
@@ -43,7 +50,7 @@
            
 	       
 	    
-        callback(list.concat(defaultList).slice(0,3));
+        //callback(list.concat(defaultList).slice(0,3));
         
 	}).execute();
 
