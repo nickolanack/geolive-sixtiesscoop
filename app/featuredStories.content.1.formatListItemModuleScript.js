263606b2e2e033d12a749bf7988331d5d7da5887
nickolanack <nickblackwell82@gmail.com>
2022-10-19 17:38:38 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/featuredStories.content.1.formatListItemModuleScript.js
+++ app/featuredStories.content.1.formatListItemModuleScript.js
@@ -1,3 +1,21 @@
 var card=child.getCard(); //index;
 var storyItem=child.getResp().story[card];
 
+if(storyItem.attributes.isBirthStory===true){
+    childView.getElement().addClass('birth-card');
+    return;
+}
+
+if(item.isRepatriationStory===true){
+    childView.getElement().addClass('repatriation-card');
+    return;
+}
+
+
+childView.getElement().addClass('journey-card');
+
+
+if(item.isAdoptionStory===true){
+    childView.getElement().addClass('adoption-card');
+
+}
