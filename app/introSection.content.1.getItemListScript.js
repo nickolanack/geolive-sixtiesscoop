07bc85192cbe347e68c3542425f98c1a91ed9125
nickolanack <nickblackwell82@gmail.com>
2022-10-19 20:16:13 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/introSection.content.1.getItemListScript.js
+++ app/introSection.content.1.getItemListScript.js
@@ -1,4 +1,10 @@
 return [
-    new MockDataTypeItem({name:"##Search\n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "className":"search"}), 
+    new MockDataTypeItem({
+        
+        click:function(){
+            document.location=document.location.hostname+'/search'
+        },
+        
+        name:"##Search\n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "className":"search"}), 
     new MockDataTypeItem({name:"##Share\n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "className":"view"}), 
     new MockDataTypeItem({name:"##Explore\n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", "className":"browse"})];
