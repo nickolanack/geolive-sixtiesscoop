0c280ca110d948692e4c25c53927b0fcbdeb0317
nickolanack <nickblackwell82@gmail.com>
2022-10-19 20:08:29 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/introSection.content.1.getItemListScript.js
+++ app/introSection.content.1.getItemListScript.js
@@ -1,4 +1,4 @@
 return [
-    new MockDataTypeItem({name:"##Search", "className":"search"}), 
+    new MockDataTypeItem({name:"##Search\n Some description", "className":"search"}), 
     new MockDataTypeItem({name:"##Share", "className":"view"}), 
     new MockDataTypeItem({name:"##Explore", "className":"browse"})];
