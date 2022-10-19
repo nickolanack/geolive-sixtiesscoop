e554069f079cfe103c0faf320f996e70fce41ec6
nickolanack <nickblackwell82@gmail.com>
2022-10-19 03:31:33 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/headerMenuContent.content.1.getItemListScript.js
+++ app/headerMenuContent.content.1.getItemListScript.js
@@ -1,4 +1,4 @@
-return [
+var links= [
     new MockDataTypeItem({name:"Home", click:function(){
         window.location.href="/"
     }}), 
@@ -34,3 +34,55 @@ return [
         
     }})
 ];
+
+
+if(AppClient.getUserType()!=="guest"){
+        
+    links.pop();
+    links.push(new MockDataTypeItem({name:"Sign out", click:function(){
+        
+        
+        
+                                            (new UIModalDialog(
+												application, 
+												new MockDataTypeItem({
+												    mutable:true,
+												    mergeFieldValues:true,
+												    // appendFieldObject:false, //is false by default if mergeFieldValues. must have toObject method
+												    updateField:"data", 
+												    data:{}, 
+												    "stepOptions":{
+												        width:700
+												    },
+												    parameters:[
+												        {
+								                	      "name": "heading",
+								                	      "description": "",
+								                	      "fieldType": "heading",
+								                	      "defaultValue": "Are you sure?,
+								                	      "options": {}
+								                	    }
+												    ]
+												}), 
+												{
+													"formName": 'genericParametersForm',
+													"formOptions": {
+														template: "form"
+													}
+												}
+											)).show(function(yes){
+                                                
+                                                if(yes){ 
+                                                    AppClient.logout(); 
+                                                    
+                                                }
+                                                
+											});
+        
+        
+    }}))
+    
+}
+
+
+return links;
