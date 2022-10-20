a39d5273ad1f36fafc41f4b2bec38d375ddcff40
nickolanack <nickblackwell82@gmail.com>
2022-10-20 19:30:49 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/gettingStarted.content.1.getItemListScript.js
@@ -0,0 +1,79 @@
+
+(new AjaxControlQuery(CoreAjaxUrlRoot, "get_configuration_field", {
+		'widget': "primaryLinkItems",
+		'field': "links"
+	})).addEvent('success',function(response){
+
+
+
+
+var configEdit = (new ConfigEdit(application, 'primaryLinkItems')).withTemplate(function(field, index){
+       return [
+												        {
+								                	      "name": "heading",
+								                	      "description": "",
+								                	      "fieldType": "heading",
+								                	      "defaultValue": "Edit Primary Link",
+								                	      "options": {}
+								                	    },   
+								                	    {
+								                	      "name": "Markdown Content",
+								                	      "description": "",
+								                	      "fieldType": "text",
+								                	      "defaultValue": response.value[index].content,
+								                	      "options": {
+								                	          "parameterName":"content",
+								                	            "className":"markdown-field"
+								                	      }
+								                	    }
+	    ]; 
+    
+}, 'genericParametersForm', {
+    
+    "labelForSubmit":"Save",
+    "showCancel":false
+    
+});
+
+
+
+    callback( [
+        new MockDataTypeItem({
+            
+            click:function(){
+                document.location=document.location.origin+'/search'
+            },
+            edit:function(){
+                configEdit.editIndex('links', 0);
+            },
+            name:response.value[0].content,
+            "className":"search"
+            
+        }), 
+        new MockDataTypeItem({
+             click:function(){
+                document.location=document.location.origin+'/share'
+            },
+            edit:function(){
+                configEdit.editIndex('links', 1);
+            },
+            name:response.value[1].content,
+            "className":"view"
+            
+        }), 
+        new MockDataTypeItem({
+             click:function(){
+                document.location=document.location.origin+'/explore'
+            },
+            edit:function(){
+                configEdit.editIndex('links', 2);
+            },
+            name:response.value[2].content,
+            "className":"browse"
+            
+        })
+    
+    ]);
+    
+    
+}).execute();
