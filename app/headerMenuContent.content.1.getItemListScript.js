efe79ef3f88848ebbb871d2cbf1efe1d11c25033
nickolanack <nickblackwell82@gmail.com>
2022-10-21 17:13:55 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/headerMenuContent.content.1.getItemListScript.js
+++ app/headerMenuContent.content.1.getItemListScript.js
@@ -89,6 +89,18 @@ if(AppClient.getUserType()!=="guest"){
         
         
     }}))
+ 
+ 
+ if(AppClient.getUserType()=="admin"){
+    new MockDataTypeItem({name:"[]", click:function(){
+        if(document.body.hasClass('hide-admin')){
+            document.body.removeClass('hide-admin');
+            return
+        }
+        document.body.addClass('hide-admin');
+    }}), 
+ }
+ 
     
 }
 
