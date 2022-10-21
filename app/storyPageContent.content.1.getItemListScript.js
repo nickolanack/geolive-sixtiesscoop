a54b5146e5ad0f6122202f9f6d275728593a888a
nickolanack <nickblackwell82@gmail.com>
2022-10-21 01:12:41 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/storyPageContent.content.1.getItemListScript.js
+++
@@ -1,8 +0,0 @@
-(new AjaxControlQuery(CoreAjaxUrlRoot, "get_story_with_item", {
-                    "plugin": "MapStory",
-                    "item": parseInt(document.location.pathname.split('story/').pop().split('/').shift())
-                })).addEvent("success", function(resp) {
-                  
-                    (new StoryUser(resp)).getCards(callback);
-                
-                }).execute();
