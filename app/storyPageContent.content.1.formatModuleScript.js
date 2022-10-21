492a75e3cc01bbe89a1d43ee5b8903546eb13aab
nickolanack <nickblackwell82@gmail.com>
2022-10-21 04:39:11 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/storyPageContent.content.1.formatModuleScript.js
@@ -0,0 +1,21 @@
+module.once('display', function(){
+    setTimeout(function(){
+        
+        var list=module.getDetailViews().reverse();
+        var rate=300;
+        var item=null;
+        var animate=function(list){
+            if(item){
+                item.removeClass('anim');
+            }
+            if(list.length){
+                item=list.shift();   
+                item.getElement().addClass('anim');
+                setTimeout(function(){
+                    animate(list);
+                }, rate);
+            }
+        }
+        
+    }, 1000);  
+})
