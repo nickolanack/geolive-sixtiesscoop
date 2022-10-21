f577fc764f36d19944089de7849cf49feaeb96f2
nickolanack <nickblackwell82@gmail.com>
2022-10-21 18:42:23 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

---
+++ app/storyIndex.content.2.resolveItemScript.js
@@ -0,0 +1,37 @@
+(new AjaxControlQuery(CoreAjaxUrlRoot, "get_story_with_item", {
+                    "plugin": "MapStory",
+                    "item": parseInt(document.location.pathname.split('story/').pop().split('/').shift())
+                })).addEvent("success", function(storyResp) {
+                  
+                  //callback(new StoryUser(storyResp));
+                   
+                   
+                   
+                    (new AjaxControlQuery(CoreAjaxUrlRoot, "get_attribute_value_list", {
+                        "plugin": "Attributes",
+                        "itemId":storyResp.features[0].uid,
+                        "itemType":"user"
+                    })).on("success", function(userResp) {
+                        
+                        if(!userResp.success){
+                             callback(new StoryUser(storyResp));
+                             return;
+                        }
+                      
+                       callback(new StoryUser(ObjectAppend_(storyResp, {
+                            user:userResp.values[0].entries[0]
+                        })));
+                       
+                    
+                    }).on('error',function(){
+                       
+                       callback(new StoryUser(storyResp));
+                       
+                       
+                    }).execute();
+                   
+                
+                }).execute();
+                
+                
+                
