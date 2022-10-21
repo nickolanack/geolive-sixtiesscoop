2767173e2f5526c3b629e471db473703fbd8fb78
Nick Blackwell <nickblackwell82@gmail.com>
2022-10-21 15:25:11 -0700

    add feature

--- Plugins/MapStory/MapStoryAjaxController.php
+++ Plugins/MapStory/MapStoryAjaxController.php
@@ -138,7 +138,7 @@ class MapStoryAjaxController extends core\AjaxController implements \core\extens
 				if((!$limit)||$index>=$limit[0]&&$index<$limit[1]){
 					$list[]=array('id'=>$result->id, 'features'=>array( $this->getPlugin()->formatFeatureMetadata($result, 'attribute_')), 'story'=>$this->getPlugin()->getUsersStoryMetadata($result->uid));
 				}else{
-					$list[]=array('id'=>$result->id);
+					$list[]=array('id'=>$result->id, 'features'=>array( $this->getPlugin()->formatFeatureMetadata($result, 'attribute_')));
 				}
 
 
