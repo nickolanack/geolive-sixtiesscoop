50c4b94d2e4918d92bdc46e870637d99aa9b6db2
Nick Blackwell <nickblackwell82@gmail.com>
2022-10-21 20:16:05 -0700

    use json limit

--- Plugins/MapStory/MapStoryAjaxController.php
+++ Plugins/MapStory/MapStoryAjaxController.php
@@ -128,18 +128,21 @@ class MapStoryAjaxController extends core\AjaxController implements \core\extens
 			}';
 
 		$index=0;
-		$limit=[0,10];
+		$limit=false;
+		if(isset($json->limit)&&is_array($json->limit)&&count($json->limit)==2){
+			$limit=[intval($json->limit[0]), intval($json->limit[1])];
+		}
 
 		(new \spatial\AttributeFeatures('storyAttributes'))
 			->withType('MapStory.card') //becuase attribute type is overriden
 			->withAllAttributes($prefix)
 			->withFilter($filterBirthStories)->iterate(function($result)use(&$list, $prefix, &$index, $limit){
 
-				//if((!$limit)||$index>=$limit[0]&&$index<$limit[1]){
-				//	$list[]=array('id'=>$result->id, 'features'=>array( $this->getPlugin()->formatFeatureMetadata($result, 'attribute_')), 'story'=>$this->getPlugin()->getUsersStoryMetadata($result->uid));
-				//}else{
+				if((!$limit)||$index>=$limit[0]&&$index<$limit[1]){
 					$list[]=array('id'=>$result->id, 'features'=>array( $this->getPlugin()->formatFeatureMetadata($result, 'attribute_')));
-				//}
+				}else{
+					$list[]=array('id'=>$result->id,);
+				}
 
 
 				$index++;
