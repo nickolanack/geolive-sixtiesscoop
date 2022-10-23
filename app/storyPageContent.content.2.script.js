b0dcd7d27f14c1101b0b973c04e4e833242a90a9
nickolanack <nickblackwell82@gmail.com>
2022-10-23 05:00:12 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/storyPageContent.content.2.script.js
+++ app/storyPageContent.content.2.script.js
@@ -4,14 +4,30 @@ var el=new Element('span',{
     "class":"media-container empty"
 });
 
-item.getVideos();
+item.getVideos(function(videos){
+    
+    if(videos.length){
+        el.appendChild(new Element('span',{
+            styles:{
+                'background-image':'url('+videos[0].poster+')'
+            },
+            "class":"video"
+        }));
+        
+        el.removeClass('empty');
+        el.addClass('has-video');
+    }
+    
+    
+});
 item.getImages(function(images){
     
     if(images.length){
         el.appendChild(new Element('span',{
             styles:{
                 'background-image':'url('+images[0].url+')'
-            }
+            },
+            "class":"image"
         }));
         
         el.removeClass('empty');
