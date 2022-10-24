fb76ab3a04a8a30e9962285006805cd01bffe0d8
nickolanack <nickblackwell82@gmail.com>
2022-10-24 06:18:38 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/searchPageContent.content.1.getAggregatorsScript.js
+++ app/searchPageContent.content.1.getAggregatorsScript.js
@@ -1,1 +1,47 @@
-return ScoopStories.getSearchAggregators(search);
+///return ScoopStories.getSearchAggregators(search);
+
+
+
+var SearchAggregator = new Class({
+    Extends: UISearchListAggregator,
+    initialize: function(search, options) {
+        var me = this;
+        this.parent(search, Object.append({
+
+            PreviousTemplate: UIListAggregator.PreviousTemplate,
+            MoreTemplate: UIListAggregator.MoreTemplate,
+            ResultTemplate: UIListAggregator.NamedViewTemplate(application, {
+                namedView: "scoopStoryDetail",
+                formatResult: function(data) {
+
+                   
+                },
+                events: {
+                    click: function() {
+
+
+                    }
+                }
+            })
+
+        }, options));
+    },
+    getLastResponse:function(){
+        return this._results;
+    },
+    _getRequest: function(filters) {
+        var me = this;
+        var string = me.currentSearchString;
+
+
+        return (new AjaxControlQuery(CoreAjaxUrlRoot, 'advanced_search', {
+                'plugin': 'MapStory',
+                'search': {name:string},
+                 searchOptions: filters
+            })).addEvent('success',function(results){
+
+            me._results=results;
+
+        });
+    }
+});
