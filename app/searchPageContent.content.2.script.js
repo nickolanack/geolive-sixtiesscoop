b3cb0eb6b58e027886ff5a2b1b3f3315a08a9b11
nickolanack <nickblackwell82@gmail.com>
2022-10-24 05:19:36 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/searchPageContent.content.2.script.js
+++ app/searchPageContent.content.2.script.js
@@ -1,1 +1,17 @@
-return new Element('button', {'class':"inline-edit options-btn", html:"Advanced"});
+return new Element('button', {'class':"inline-edit options-btn", html:"Advanced", "events":{
+    "click":function(){
+        
+        if(this.hasClass('enabled')){
+                
+            this.removeClass('enabled');
+            $$('.ui-view.search-content')[0].removeClass('enabled');
+            
+            return;
+        }
+        
+        
+        this.addClass('enabled');
+        $$('.ui-view.search-content')[0].addClass('enabled');
+        
+    }
+}});
