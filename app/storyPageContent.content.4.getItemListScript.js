dcbfd59c3e0e2773c60aabd55a0ef3fdfe75c74c
nickolanack <nickblackwell82@gmail.com>
2022-11-01 03:46:43 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/storyPageContent.content.4.getItemListScript.js
+++ app/storyPageContent.content.4.getItemListScript.js
@@ -1,111 +1,24 @@
 var links= [
-    new MockDataTypeItem({name:"Home", link:"/", click:function(){
-        window.location.href="/"
+    new MockDataTypeItem({name:"Share", click:function(){
+        
     }}), 
-    new MockDataTypeItem({name:"Map", link:"/map", click:function(){
-        window.location.href="/map"
+    new MockDataTypeItem({name:"Contact", click:function(){
+        
     }}), 
-    new MockDataTypeItem({name:"Search",  link:"/search", click:function(){
-        window.location.href="/search"
-    }}), 
-    new MockDataTypeItem({name:"Help", link:"/help" }), 
-    new MockDataTypeItem({name:"About", link:"/about" }), 
-    new MockDataTypeItem({name:"Sign in", click:function(){
-        
-        
-                                             (new UIModalDialog(
-												application, 
-												AppClient, 
-												{
-													"formName": 'loginFormView',
-													"formOptions": {
-														template: "form"
-													}
-												}
-											)).show(function(){
-
-					            			
-
-											}).on('complete', function(item){
-											    
-											    
-											    
-											});
-        
-        
-        
-        
-    }})
+   
 ];
 
 
 if(AppClient.getUserType()!=="guest"){
         
-    links.pop();
-    links.push(new MockDataTypeItem({name:"Sign out", click:function(){
-        
-        
-        
-                                            (new UIModalDialog(
-												application, 
-												new MockDataTypeItem({
-												    mutable:true,
-												    mergeFieldValues:true,
-												    // appendFieldObject:false, //is false by default if mergeFieldValues. must have toObject method
-												    updateField:"data", 
-												    data:{}, 
-												    "stepOptions":{
-												        width:700
-												    },
-												    parameters:[
-												        {
-								                	      "name": "heading",
-								                	      "description": "",
-								                	      "fieldType": "heading",
-								                	      "defaultValue": "Are you sure?",
-								                	      "options": {}
-								                	    }
-												    ]
-												}), 
-												{
-													"formName": 'genericParametersForm',
-													"formOptions": {
-														template: "form",
-														"className": "alert-view",
-                        								"showCancel":true,
-                        								"labelForSubmit":"Yes, log me out",
-                        								"labelForCancel":"Cancel",
-                        								"closable":true
-													}
-												}
-											)).show(function(yes){
-                                                
-                                                if(yes){ 
-                                                    AppClient.logout(); 
-                                                    
-                                                }
-                                                
-											});
-        
-        
-    }}))
- 
+
  
  if(AppClient.getUserType()=="admin"){
-    links.push(new MockDataTypeItem({name:"[]", className:"admin-toggle", click:function(){
-        if(document.body.hasClass('hide-admin')){
-            document.body.removeClass('hide-admin');
-            return
-        }
-        document.body.addClass('hide-admin');
+    links.push(new MockDataTypeItem({name:"Edit story", click:function(){
+       
     }}));
  }
  
-  if(AppClient.getUserType()!="guest"){
-      links.splice(1,0,new MockDataTypeItem({name:"My story",  link:"/me", click:function(){
-        window.location.href="/me"
-    }}))
-  }
  
     
 }
