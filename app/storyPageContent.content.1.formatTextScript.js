4642320090dd0ce7110770252c7a77dc714327ca
nickolanack <nickblackwell82@gmail.com>
2022-11-01 19:15:26 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/storyPageContent.content.1.formatTextScript.js
+++ app/storyPageContent.content.1.formatTextScript.js
@@ -1,3 +1,16 @@
-module.getElement().addEvent('click',function(){
-    window.location.href="/map/story/"+item._storyData[0].id
-})
+
+
+if(item._storyData.length>0){
+
+    module.getElement().href="/map/story/"+item._storyData[0].id;
+    module.getElement().addEvent('click',function(){
+        window.location.href="/map/story/"+item._storyData[0].id
+    })
+    
+    return;
+
+}
+
+
+ module.getElement().addClass('empty-story');
+ return 'This user does not have a story';
