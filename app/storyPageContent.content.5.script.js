05491e88954cb6e824c66960e9160943ccf31c23
nickolanack <nickblackwell82@gmail.com>
2022-11-01 17:22:04 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/storyPageContent.content.5.script.js
+++ app/storyPageContent.content.5.script.js
@@ -1,3 +1,15 @@
-return new Element('button',{"class":"featured-link", "click":function(){
+
+var el=new Element('button',{"class":"featured-link", "click":function(){
     
 }});
+(new AjaxControlQuery(CoreAjaxUrlRoot, "get_configuration_field", {
+		'widget': "featuredStoriesItems",
+		'field': "featured"
+	})).addEvent('success',function(response){
+	    
+	    console.log(response);
+	    console.log(item);
+	    
+	})
+
+return el;
