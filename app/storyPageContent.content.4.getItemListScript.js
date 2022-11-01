4a25936bf11ac666a4f60388d71a6e3fc4dfc712
nickolanack <nickblackwell82@gmail.com>
2022-11-01 04:22:36 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/storyPageContent.content.4.getItemListScript.js
+++ app/storyPageContent.content.4.getItemListScript.js
@@ -16,6 +16,31 @@ if(AppClient.getUserType()!=="guest"){
  if(AppClient.getUserType()=="admin"){
     links.push(new MockDataTypeItem({name:"Edit story", "className":"edit", click:function(){
        
+       
+       
+       (new UIModalDialog(
+												application, 
+												AppClient, 
+												{
+													"formName": 'combinedForm',
+													"formOptions": ObjectAppend_({
+														template: "form",
+														labelForCancel:Cancel,
+														labelForSave:"Save"
+													}, formOptions)
+												}
+											)).show(function(){
+
+
+											}).on('complete', function(item){
+											    
+											   
+											    
+											});
+       
+       
+       
+       
     }}));
  }
  
