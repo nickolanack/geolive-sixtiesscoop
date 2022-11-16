dda8306b94facaa83ff7b96176158388382ed1ef
nickolanack <nickblackwell82@gmail.com>
2022-11-16 23:22:57 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/storyPageContent.content.4.getItemListScript.js
+++ app/storyPageContent.content.4.getItemListScript.js
@@ -14,7 +14,7 @@ var links= [
             
             (new SocialLinks()).linkElement(
                 module.getElement().appendChild(
-                    new Element('a',{"class":"fb-share", html:"Share on facebook", dataset:{
+                    new Element('a',{"class":"social-share fb-share", html:"Share on facebook", dataset:{
                         'share':"facebook"
                         
                     }})),
@@ -22,7 +22,7 @@ var links= [
             
             (new SocialLinks()).linkElement(
                 module.getElement().appendChild(
-                    new Element('a',{"class":"fb-share", html:"Share on twitter", dataset:{
+                    new Element('a',{"class":"social-share twitter-share", html:"Share on twitter", dataset:{
                         'share':"twitter"
                     }})),
                 {});
@@ -30,7 +30,7 @@ var links= [
                 
            
                 module.getElement().appendChild(
-                    new Element('a',{"class":"email-share", html:"Share via email", 
+                    new Element('a',{"class":"social-share email-share", html:"Share via email", 
                         'target':"_blank",
                         'href':"mailto:?subject="+
                             encodeURIComponent('Check out '+listModule.getListItem().getUsersName()+'\'s story on the sixties-scoop network')+"&body="+
