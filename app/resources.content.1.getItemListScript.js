eccfb34eb77f26cab4245a10e9ece3aff7e83751
nickolanack <nickblackwell82@gmail.com>
2022-11-18 23:22:04 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/resources.content.1.getItemListScript.js
+++ app/resources.content.1.getItemListScript.js
@@ -1,7 +1,8 @@
 var links= [
-    new MockDataTypeItem({name:"Home", link:"/", click:function(){
-        window.location.href="/"
-    }})
+    new MockDataTypeItem({
+        name:"Interactive map hopes to help Sixties Scoop survivors find their family", 
+        link:"https://www.standard-freeholder.com/news/local-news/interactive-map-hopes-to-help-sixties-scoop-survivors-find-their-family"
+    })   
    
  
 ];
