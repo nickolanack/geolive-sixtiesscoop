1eedc9f0f2b65c2b920df2c2dc10e41db85a766a
nickolanack <nickblackwell82@gmail.com>
2022-11-18 23:28:50 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/resources.content.1.getItemListScript.js
+++ app/resources.content.1.getItemListScript.js
@@ -7,10 +7,16 @@ var links= [
         name:"Reconnecting After Suffering a Kidnapping Trauma: The Sixties Scoop", 
         link:"https://mediaquotientinc.com/reconnecting-after-suffering-a-kidnapping-trauma-the-sixties-scoop/"
     }),
-     new MockDataTypeItem({
+    new MockDataTypeItem({
         name:"prezi: The 60's Scoop", 
-        link:" https://prezi.com/p/610ygi8a2gx3/the-60s-scoop/"
-    })
+        link:"https://prezi.com/p/610ygi8a2gx3/the-60s-scoop/"
+    }),
+    new MockDataTypeItem({
+        name:"Facebook: Adopted Native American's", 
+        link:"https://ms-my.facebook.com/permalink.php?story_fbid=1459170797762121&id=665027630509779/"
+    }),
+    
+    
     
     
     
