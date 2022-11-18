6039d274696914e8e193c53fa212cc29910ff88c
nickolanack <nickblackwell82@gmail.com>
2022-11-18 23:26:30 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/resources.content.1.getItemListScript.js
+++ app/resources.content.1.getItemListScript.js
@@ -2,7 +2,19 @@ var links= [
     new MockDataTypeItem({
         name:"Interactive map hopes to help Sixties Scoop survivors find their family", 
         link:"https://www.standard-freeholder.com/news/local-news/interactive-map-hopes-to-help-sixties-scoop-survivors-find-their-family"
-    })   
+    }),
+    new MockDataTypeItem({
+        name:"Reconnecting After Suffering a Kidnapping Trauma: The Sixties Scoop", 
+        link:"https://mediaquotientinc.com/reconnecting-after-suffering-a-kidnapping-trauma-the-sixties-scoop/"
+    }),
+     new MockDataTypeItem({
+        name:"prezi: The 60's Scoop", 
+        link:" https://prezi.com/p/610ygi8a2gx3/the-60s-scoop/"
+    })
+    
+    
+    
+   
    
  
 ];
