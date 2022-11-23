04566b597933df48ee7b879024226da03a73e667
nickolanack <nickblackwell82@gmail.com>
2022-11-23 23:53:31 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/headerMenuContent.content.1.formatListItemModuleScript.js
+++ app/headerMenuContent.content.1.formatListItemModuleScript.js
@@ -1,3 +1,9 @@
 if(child.getLink){
     childView.getElement().href=child.getLink();
+
+}
+
+if(child.getHidden&&child.getHidden()===true){
+    //hidden menu, visible to bots
+    childView.getElement().addClass('hidden');
 }
