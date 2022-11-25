7ffbefd315c6ae0387d9a13afeb9810d63fb2279
nickolanack <nickblackwell82@gmail.com>
2022-11-25 06:35:16 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/combinedForm.1.content.8.formatModuleScript.js
+++ app/combinedForm.1.content.8.formatModuleScript.js
@@ -2,4 +2,9 @@ module.getChildWizard(function(wizard) {
 						wizard.addEvent('valueChange', function() {
 							module.getWizard().setDataValue({'repatriation':wizard.getData()});
 						});
+						
+						module.getWizard().addDataAggregationFunction(module.getWizard().getCurrentIndex(), function(){
+        					module.getWizard().setDataValue({'repatriation':wizard.getData()});
+        				});
+						
 					});
