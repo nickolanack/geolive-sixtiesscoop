3345ccd1e82806534aefb11120ce35c58be8110d
nickolanack <nickblackwell82@gmail.com>
2022-12-01 16:30:47 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/loginFormView.1.content.4.script.js
+++ app/loginFormView.1.content.4.script.js
@@ -3,8 +3,8 @@ return new ModalFormButtonModule(application, AppClient, {
                 label: "Register",
                 formName: "registerFormView",
                 "class": "secondary-btn register",
-                "style":"margin-left: calc( 50% - 40px );"
-    
+                "style":"margin-left: calc( 50% - 40px );",
+                formOptions: {template:"form"}
         
          }).addEvent("show",function(newWizard){
              wizard.close();
