ac04bb1a48d173e4c8b19d4351d2eb20da65fd2d
nickolanack <nickblackwell82@gmail.com>
2022-12-01 16:36:00 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/loginFormView.1.content.4.script.js
+++ app/loginFormView.1.content.4.script.js
@@ -1,4 +1,7 @@
-return new ModalFormButtonModule(application, AppClient, {
+return [
+    
+    
+    new ModalFormButtonModule(application, AppClient, {
              
                 label: "Register",
                 formName: "registerFormView",
@@ -8,4 +11,22 @@ return new ModalFormButtonModule(application, AppClient, {
         
          }).addEvent("show",function(newWizard){
              wizard.close();
+         }),
+         
+         
+    new ModalFormButtonModule(application, AppClient, {
+             
+                label: "Forgot Password",
+                formName: "forgotPassowrdFormView",
+                "class": "secondary-btn forgot=pwd",
+                "style":"margin-right: calc( 50% - 40px );",
+                formOptions: {template:"form"}
+        
+         }).addEvent("show",function(newWizard){
+             wizard.close();
          })
+         
+         
+         
+         
+    ];
