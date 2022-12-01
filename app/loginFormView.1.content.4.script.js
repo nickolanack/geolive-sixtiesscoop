9f0bc881790a1f6b47274de98e18f854dccb4102
nickolanack <nickblackwell82@gmail.com>
2022-12-01 17:31:10 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/loginFormView.1.content.4.script.js
+++ app/loginFormView.1.content.4.script.js
@@ -1,3 +1,38 @@
+
+
+var LoginItem=new Class({
+    Extends:DataTypeObject,
+    Implements:Events,
+    setSendMagicLink:function(){
+        var me=this;
+        me._task="send_magic_link";
+    },
+    setSendReset:function(){
+        var me=this;
+        me._task="send_reset";
+        
+    },
+    setEmail:function(e){
+        var me=this;
+        me._email=e;
+    },
+    save:function(cb){
+        
+        var me=this;
+        
+        (new AjaxControlQuery(CoreAjaxUrlRoot, me._task, {
+		  'plugin': "Users",
+		  'email':me._email
+		})).addEvent('success',function(){
+		    cb(true);
+		}).execute(); 
+		
+		
+
+    }
+})
+
+
 return [
     
     
@@ -14,7 +49,7 @@ return [
          }),
          
          
-    new ModalFormButtonModule(application, AppClient, {
+    new ModalFormButtonModule(application, new LoginItem(), {
              
                 label: "Forgot Password",
                 formName: "forgotPasswordForm",
