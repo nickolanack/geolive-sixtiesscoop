a2995a6a6cea721d4caed3ed5c0ecd6ce67da4fd
Nick Blackwell <nickblackwell82@gmail.com>
2022-12-04 20:07:41 -0800

    implement full filter

--- Plugins/MapStory/js/StoryFilter.js
+++ Plugins/MapStory/js/StoryFilter.js
@@ -1,6 +1,57 @@
 var StoryFilter=(function(){
 
 
+
+	var isBoolAttributeTrue=function(attr){
+		return attr===true||attr==="true";
+	}
+
+	var locationDataAttributeContainsCode=function(attr, code){
+
+		if(typeof attr=='string'&&attr[0]==='{'){
+			attr=JSON.parse(attr);
+		}
+
+		if(!(attr.geocode&&attr.geocode.address_components)){
+			return false;
+		}
+
+
+		/**
+		{
+		    "address_components": [
+		       ...
+		        {
+		            "long_name": "Canada",
+		            "short_name": "CA",
+		            "types": [
+		                "country",
+		                "political"
+		            ]
+		        },
+		        ...
+		    ],
+		    ...
+		}
+		 */
+		
+		return attr.geocode.address_components.filter(function(address){
+
+			if(address.short_name.toLowerCase()===code.toLowerCase()){
+				return true;
+			}
+
+			if(address.long_name.toLowerCase().split(' ').join('_')===code.toLowerCase().split(' ').join('_')){
+				return true;
+			}
+
+			return false;
+
+		}).length>0;
+
+
+	};
+
 	var StoryFilter=new Class_({
 
 		initialize:function(){
@@ -42,7 +93,38 @@ var StoryFilter=(function(){
 
 			if(sources.length>0||dests.length>0){
 				this._filter=function(result){
-					return true;
+
+					return result.features.filter(function(feature){
+
+						if(!isBoolAttributeTrue(feature.attributes.movesOutOfProvince)){
+							return false;
+						}
+
+
+						if(sources.length>0){
+							if(sources.filter(function(source){
+
+								return locationDataAttributeContainsCode(feature.attributes.locationData, source);
+
+							}).length>0){
+								return true;
+							}
+						}
+
+						if(dests.length>0){
+							if(dests.filter(function(dest){
+
+								return locationDataAttributeContainsCode(feature.attributes.locationDataNext, dest);
+
+							}).length>0){
+								return true;
+							}
+						}
+
+
+					}).length>0; //if any feature in story passes filter, return story
+
+					
 				}
 			}
 			return this;
