80d8f7d182687a255ef54b956c48a8d5579f61a5
Nick Blackwell <nickblackwell82@gmail.com>
2022-12-05 15:28:15 -0800

    print

--- Plugins/MapStory/MapStoryAjaxController.php
+++ Plugins/MapStory/MapStoryAjaxController.php
@@ -167,11 +167,14 @@ class MapStoryAjaxController extends core\AjaxController implements \core\extens
 			foreach ($locationData->geocode->address_components as $address) {
 				$short=strtolower(str_replace(' ', '_',  $address->short_name));
 				if($short===$code){
+
+					error_log($short.' == '.$code);
 					return true;
 				}
 
 				$long=strtolower(str_replace(' ', '_',  $address->long_name));
 				if($long===$long){
+					error_log($long.' == '.$code);
 					return true;
 				}
 			}
