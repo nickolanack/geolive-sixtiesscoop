131e4309bc39cba2871966efddd842f4e35564c4
Nick Blackwell <nickblackwell82@gmail.com>
2022-12-04 19:46:42 -0800

    add initial filter def

--- Plugins/MapStory/js/StoryFilter.js
+++ Plugins/MapStory/js/StoryFilter.js
@@ -5,11 +5,46 @@ var StoryFilter=(function(){
 
 		initialize:function(){
 
+			this._filter=function(){
+				return true;
+			}
 
 		},
 
 		fromUrl:function(){
 
+			var filter=document.location.pathname.split('/filter').pop();
+			var stubs=filter.split('/');
+
+			if(stubs.length<2){
+				return this;
+			}
+
+
+			var union=stubs.shift().indexOf('any')>=0;
+
+			var sources=[];
+			var dests=[];
+
+			stubs.forEach(function(filter){
+
+				if(filter.indexOf('source-')===0){
+					sources=sources.concat(filter.split('source-').slice(1).split('-'));
+				}
+
+				if(filter.indexOf('dest-')===0){
+					dests=dests.concat(filter.split('dest-').slice(1).split('-'));
+				}
+
+
+			});
+
+
+			if(sources.length>0||dests.length>0){
+				this._filter=function(result){
+					return true;
+				}
+			}
 			return this;
 		},
 		filterList:function(list, cb){
@@ -22,7 +57,7 @@ var StoryFilter=(function(){
 	});
 
 
-	return StoryFilter;
+	return StoryFilter;Editor.RenderStaticPreview(assetPath : string, subAssets : Object[], width : int, height : int)
 
 
 
