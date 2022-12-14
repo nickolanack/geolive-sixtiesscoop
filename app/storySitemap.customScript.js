b1ec51ed69febc9966f021221b5835f6bae747d6
nickolanack <nickblackwell82@gmail.com>
2022-12-14 18:58:52 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/storySitemap.customScript.js
+++ app/storySitemap.customScript.js
@@ -3,7 +3,7 @@
 	
 	<?php 
 
-	 foreach(GetPlugin('Maps')->listStories() as $story){
+	 foreach(GetPlugin('MapStory')->listStories() as $story){
 	     
 	     
 	     echo json_ecode($story, JSON_PRETTY_PRINT)."\n\n";
