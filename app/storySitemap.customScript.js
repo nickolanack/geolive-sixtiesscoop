4ccdd80bd90323217e722602c11b7f5b73f93eae
nickolanack <nickblackwell82@gmail.com>
2022-12-14 19:04:14 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/storySitemap.customScript.js
+++ app/storySitemap.customScript.js
@@ -2,9 +2,21 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 	
 	<?php 
+	
+	$root=HtmlDocument()->website().'/story/';
 
 	 foreach(GetPlugin('MapStory')->listStories() as $story){
+	    
+	        $path=$root.$story['id'];
+	        $date=explode(' ',$story['features'][0][modificationDate])[0];
+	 
+	     ?>
+	        <url>
+				<loc><?php echo $path?></loc>
+				<lastmod><?php echo $date;?></lastmod>
+			</url>
 	     
+	     <?php
 	     
 	     echo json_encode($story, JSON_PRETTY_PRINT)."\n\n";
 	 }
