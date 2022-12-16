f1f829e9d0262dbfbf040124449c59b93d432b92
nickolanack <nickblackwell82@gmail.com>
2022-12-16 07:22:29 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/story.onRenderApplictionScript.js
+++ app/story.onRenderApplictionScript.js
@@ -1,3 +1,10 @@
 
-//('story', 'headerMenuContent', 'bottomSection', 'placeholderLabelItem', 'storyPageContent', 'sidePanelInfoDetail');
+(new \core\extensions\widget\WidgetRenderer($targetInstance))->addInlineViews(array(
+    'story', 
+    'headerMenuContent', 
+    'bottomSection', 
+    'placeholderLabelItem', 
+    'storyPageContent', 
+    'sidePanelInfoDetail'
+));
 GetWidget('home')->renderDependencies($targetInstance);
