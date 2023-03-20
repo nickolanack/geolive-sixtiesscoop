109fc456f2f76392a296f3f116120bc4a86485d9
nickolanack <nickblackwell82@gmail.com>
2023-03-20 20:55:06 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/profileFormView.1.content.1.getValueScript.js
+++ app/profileFormView.1.content.1.getValueScript.js
@@ -1,4 +1,7 @@
  try{
+         if(item.getType()==='user'&&item.getId()==AppClient.getId()){
+         return AppClient.options.metadata.username;
+    }
  return item.options.metadata.username;
  }catch(e){
      console.warn(e);
--- app/profileFormView.1.content.1.onRenderTextFieldScript.js
+++ app/profileFormView.1.content.1.onRenderTextFieldScript.js
@@ -1,10 +1,6 @@
 inputElement.disabled=true;
 
 try{
-    
-    if(item.getType()==='user'&&item.getId()==AppClient.getId()){
-         return AppClient.options.metadata.username;
-    }
      
     if(item.options.metadata.username==item.options.metadata.email){
         module.getElement().addClass('hidden');
