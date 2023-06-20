bb1b3a58a43ea1401470042eb916943704c1e8de
nickolanack <nickblackwell82@gmail.com>
2023-06-20 17:01:50 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/story.onRenderApplictionScript.js
+++ app/story.onRenderApplictionScript.js
@@ -10,12 +10,29 @@
 GetWidget('home')->renderDependencies($targetInstance);
 
 
-// $url=$_SERVER['REQUEST_URI'];
-// $url=explode('/', $url);
-// $url=array_pop($url);
+$url=$_SERVER['REQUEST_URI'];
+$url=explode('/', $url);
+$url=array_pop($url);
 
-// $url=explode('?', $url);
-// $url=array_shift($url);
+$url=explode('?', $url);
+$url=array_shift($url);
+
+if(is_numeric($url)){
+    
+    $plugin=GetPlugin('MapStory');
+    
+    $list=$plugin->getFeaturesMetadata($json->item);
+		$user=$list[0]['uid'];
+
+		if(!is_numeric($user)){
+
+		}
+
+		//features is for debug
+		array('features'=>$list, 'story'=>$plugin->getUsersStoryMetadata($user), 'user'=>(is_numeric($user)?$plugin->getUsersMetadata($user):null));
+    
+    
+}
 
 // \HtmlDocument()->META($url, "title");
 
@@ -27,4 +44,3 @@ GetWidget('home')->renderDependencies($targetInstance);
 // <meta property="og:description"   content="Your description" />
 // <meta property="og:image"         content="https://www.your-domain.com/path/image.jpg" />
 
-\HtmlDocument()->META("Blanks story", "title");
