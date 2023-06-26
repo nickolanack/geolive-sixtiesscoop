a7ff2c23ca27b611e7a7e17ba8e223bd616967b4
nickolanack <nickblackwell82@gmail.com>
2023-06-26 05:21:52 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/toggleFeatured.customScript.js
+++ app/toggleFeatured.customScript.js
@@ -1,5 +1,17 @@
 
-return GetConfig('featuredStoriesItems')->getConfigurationValue('featured');
+//return GetConfig('featuredStoriesItems')->getConfigurationValue('featured');
+
+
+$list=$this->getPlugin()->getFeaturesMetadata($json->item);
+		$user=$list[0]['uid'];
+
+		if(!is_numeric($user)){
+
+		}
+
+		//features is for debug
+		return  array('features'=>$list, 'story'=>$this->getPlugin()->getUsersStoryMetadata($user), 'user'=>(is_numeric($user)?$this->getPlugin()->getUsersMetadata($user):null));
+
 
 
 return true;
