721e0c5195b8d53bc00e6e52fe7c68275aa43819
nickolanack <nickblackwell82@gmail.com>
2023-07-03 21:15:47 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/plugin.Maps.17.layerDisplayFormatScript.js
+++ app/plugin.Maps.17.layerDisplayFormatScript.js
@@ -9,6 +9,10 @@ $feature['info']=array(
 );
 $feature['icon']=str_replace('php-core-app/users_files','uploads-sixtiesscoop.geoforms.ca', $feature['icon']);
 
-error_log(print_r($feature,true));
+
+if( isset($feature['data']['storyAttributes']['isBirthStory'])&&$feature['data']['storyAttributes']['isBirthStory']===true&&
+    isset($feature['data']['storyAttributes']['hasStoryVideos'])&&$feature['data']['storyAttributes']['hasStoryVideos']===true){
+    error_log(print_r($feature,true));
+}
 
 return $feature;
