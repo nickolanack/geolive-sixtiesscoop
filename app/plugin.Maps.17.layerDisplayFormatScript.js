13fb512812f714bbc4537335083ff9807065d4e6
nickolanack <nickblackwell82@gmail.com>
2023-07-03 21:20:36 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/plugin.Maps.17.layerDisplayFormatScript.js
+++ app/plugin.Maps.17.layerDisplayFormatScript.js
@@ -10,9 +10,20 @@ $feature['info']=array(
 $feature['icon']=str_replace('php-core-app/users_files','uploads-sixtiesscoop.geoforms.ca', $feature['icon']);
 
 
-if( isset($feature['data']['storyAttributes']['isBirthStory'])&&$feature['data']['storyAttributes']['isBirthStory']===true&&
-    isset($feature['data']['storyAttributes']['hasStoryVideos'])&&$feature['data']['storyAttributes']['hasStoryVideos']===true){
-    error_log(print_r($feature,true));
+if( isset($feature['data']['storyAttributes']['isBirthStory'])&&
+    isset($feature['data']['storyAttributes']['hasStoryVideos'])){
+        
+    $birthStory=$feature['data']['storyAttributes']['isBirthStory'];
+    $hasStoryVideos=$feature['data']['storyAttributes']['hasStoryVideos'];
+    
+    if(($birthStory==="true"||$birthStory===true)
+        &&($hasStoryVideos$feature==="true"||$hasStoryVideos$feature===true)){
+        
+         error_log(print_r($feature,true));
+        
+    }
+        
+   
 }
 
 return $feature;
