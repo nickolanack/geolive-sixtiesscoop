6d902bb83d0abb307e4d13dc3803eb9859c5f67b
nickolanack <nickblackwell82@gmail.com>
2023-07-03 21:26:16 +0000

    administrator -- Auto Commit - Changes made through Frontend Interface

--- app/plugin.Maps.17.layerDisplayFormatScript.js
+++ app/plugin.Maps.17.layerDisplayFormatScript.js
@@ -18,7 +18,7 @@ if( isset($feature['data']['storyAttributes']['isBirthStory'])&&
     
     if(($birthStory==="true"||$birthStory===true)
         &&($hasStoryVideos==="true"||$hasStoryVideos===true)){
-        
+         $feature['icon']="https://nickolanackbucket.s3.us-west-2.amazonaws.com/sixtiesscoop.geoforms.ca/1/Uploads/0Zu_%5BImAgE%5D_MgG_%5BG%5D_1LQ.png";
          error_log(print_r($feature,true));
         
     }
